2d6bb511308bde7150d1bd7a958fe7298ce9493f
woojinzz <dnwls3984@gmail.com>
2025-02-11 08:58:14 +0900

    25-02-11 프로젝트 시작

---
+++ src/main/java/com/koreaIT/jsp/am/HomeMainServlet.java
@@ -0,0 +1,24 @@
+package com.koreaIT.jsp.am;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/home/main")
+public class HomeMainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public HomeMainServlet() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8;");
+		response.getWriter().append("ㅎㄴ <br />");
+		response.getWriter().append("ㅎㄴ 테스트<br />");
+		
+	}
+	
+}
